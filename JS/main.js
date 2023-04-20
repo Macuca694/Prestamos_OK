3d43a4a64a1c4c6769442f1b9b148e5bc48cf33e
Macarena Lara <macarenalara@live.com.ar>
2023-04-19 22:28:49 -0300

    se agregaron objetos y arrays

--- JS/main.js
+++ JS/main.js
@@ -16,5 +16,28 @@ for (let turno = 1; turno <= numeroClientes; turno++)  {
     let nombre = prompt ("¿Cuál es su nombre" + turno + "?");
     alert ("Perfecto, " + nombre + ", se le asignará el turno número"  +  turno + " para que un asesor se ponga en contacto");
     console.log ("Al cliente " + nombre + " le corresponde el turno número" + turno);
-}
+};
 
+//Definición de características objeto//
+let Nombre
+let apellido
+let edad
+let cuil 
+
+//creacion de molde//
+let unCliente  =	{
+ Nombre: "Griselda",
+ apellido: "Gonzalez",
+ edad:  "18",
+ cuil:"27384119471", 
+};
+
+console.log ("el objeto se compone de estos datos", unCliente);
+alert ("el objeto se compone de estos datos" + unCliente);
+
+console.log ("El cliente" + unCliente.nombre + "será calificado para sacar un préstamo");
+alert ("El cliente"+ unCliente + "se llama" ["nombre"]);
+
+//Declaración de Arrays
+const ARRAY_DE_NUMEROS = [10,20,30,40,50]
+console.log ("Qué hay dentro del array en la posición 25?", ARRAY_DE_NUMEROS [25]);
